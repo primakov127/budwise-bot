c4558056243bc7d158d9246a3d03da7f55e5fe67
primakov127 <primakov127@gmail.com>
2024-08-08 23:43:22 +0200

    Remove load_env execution

--- db.py
+++ db.py
@@ -2,12 +2,8 @@ import os
 
 import motor.motor_asyncio
 from beanie import init_beanie
-from dotenv import load_dotenv
 
 from models import Expense
-
-# Load environment variables from .env file
-load_dotenv()
 
 password = os.getenv("MONGODB_PASSWORD")
 connection_string = f"mongodb+srv://admin:{password}@primarycluster.3lfoyaq.mongodb.net/?retryWrites=true&w=majority&appName=PrimaryCluster"
